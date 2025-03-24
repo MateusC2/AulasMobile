e1fe704063cff5479463dbc72c1792d6c7132be8
MateusC2 <mateuscubeiro.educ@gmail.com>
2025-03-24 13:36:53 -0300

    add ocultar senha

--- meuAppp/src/screens/Login.js
+++ meuAppp/src/screens/Login.js
@@ -10,51 +10,61 @@ import {
 } from "react-native";
 import api from "../axios/axios";
 import Cadastro from "./Cadastro";
+import { Ionicons } from "@expo/vector-icons";
 
-export default function Login({navigation}) {
+export default function Login({ navigation }) {
   const [user, setUser] = useState({
     email: "",
     password: "",
+    showPassord: false,
   });
 
   async function handleLogin() {
     await api.postLogin(user).then(
-        (response)=>{
-            console.log(response.data.message)
-            Alert.alert('OK',response.data.message)
-            navigation.navigate("Home")
-        },(error)=>{
-            console.log(error.response.data.error)
-            Alert.alert('Erro', error.response.data.error)
-        }
-    )
+      (response) => {
+        console.log(response.data.message);
+        Alert.alert("OK", response.data.message);
+        navigation.navigate("Home");
+      },
+      (error) => {
+        console.log(error.response.data.error);
+        Alert.alert("Erro", error.response.data.error);
+      }
+    );
   }
-
-
 
   return (
     <View style={styles.container}>
       <Text style={styles.tittle}>Faça Login</Text>
       <TextInput
-      style={styles.input}
+        style={styles.input}
         placeholder="Email"
         value={user.email}
         onChangeText={(value) => {
           setUser({ ...user, email: value });
         }}
       ></TextInput>
-      <TextInput
-      style={styles.input}
-        placeholder="Senha"
-        value={user.password}
-        onChangeText={(value) => {
-          setUser({ ...user, password: value });
-        }}
-      ></TextInput>
+      <View style={styles.passwordContainer}>
+        <TextInput
+          style={styles.passwordInput}
+          placeholder="Senha"
+          value={user.password}
+          secureTextEntry={user.showPassord}
+          onChangeText={(value) => {
+            setUser({ ...user, password: value });
+          }}
+        ></TextInput>
+        <TouchableOpacity onPress={()=> setUser({...user,showPassord:!user.showPassord})}>
+          <Ionicons name={user.showPassord?"eye-off":"eye"} size={24} color="gray" />
+        </TouchableOpacity>
+      </View>
       <TouchableOpacity onPress={handleLogin} style={styles.button}>
         <Text>Entrar</Text>
       </TouchableOpacity>
-      <Button title="Cadastro" onPress={()=> navigation.navigate("Cadastro")}/>
+      <Button
+        title="Cadastro"
+        onPress={() => navigation.navigate("Cadastro")}
+      />
     </View>
   );
 }
@@ -69,16 +79,27 @@ const styles = StyleSheet.create({
     fontSize: 28,
     fontWeight: "bold",
   },
-  input:{
-    width:"100%",
-    height:40,
-    borderBottomWidth:1,
-    marginBottom:10,
-    paddingHorizontal:10
+  input: {
+    width: "100%",
+    height: 40,
+    borderBottomWidth: 1,
+    marginBottom: 10,
+    paddingHorizontal: 10,
   },
-  button:{
-    backgroundColor:"green",
-    padding:10,
-    borderRadius:5
+  button: {
+    backgroundColor: "green",
+    padding: 10,
+    borderRadius: 5,
+  },
+  passwordContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    width: "100%",
+    borderBottomWidth:1,
+    paddingRight: 10,
+  },
+  passwordInput:{
+    flex:1,
+    height:40,
   }
 });
